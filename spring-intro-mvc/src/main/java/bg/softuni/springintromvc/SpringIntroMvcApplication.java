c188bd65516d5424c87e5193ef0fce09e2d2a00d
adriana-gaydova <adriana.t.gaydova@gmail.com>
2022-06-06 16:32:39 +0300

    Uploading demo for cookies and session.

---
+++ spring-intro-mvc/src/main/java/bg/softuni/springintromvc/SpringIntroMvcApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.springintromvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroMvcApplication.class, args);
+	}
+
+}
